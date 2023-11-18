8aecda91b3ca5fa318acadc7e7f34bbf92efc5a6
Dominy Martins <122042813+DominyMartins@users.noreply.github.com>
2023-11-18 10:11:21 -0300

    Create infos.php

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "database-1.cwnkmryuxnjb.sa-east-1.rds.amazonaws.com";
+$username = "dominymartins";
+$password = "qX2qdp8FQXTk5LwutWlS";
+$database = "DOMINYDB";
+?>
